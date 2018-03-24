267672b9d086b863950080c124a3d58478cc0e93
Alexander Florian-Prescott <31100098+EPluribusAlex@users.noreply.github.com>
2018-03-24 19:17:08 -0400

    server file, config file, pachages saved, index object, readme updates, dotenv implemented (#8)

---
+++ models/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const fs = require("fs"),
+			path = require("path"),
+			Sequelize = require("sequelize");
+
+const basename = path.basename(module.filename),
+			env = process.env.NODE_ENV || "development",
+			config = require(__dirname + "/../config/config.js")[env];
+
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => {
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+  })
+  .forEach((file) => {
+    const model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if(db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
