7c4af76799c4f754be572adce2a9164b63183db1
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-03-25 13:02:08 -0500

    added basic sms controller logic

---
+++ controller/sms.js
@@ -0,0 +1,88 @@
+'use strict'
+
+// do I have to required this if this is required in server.js?
+require('dotenv').config()
+
+//reverse carrier lookup for sending zero cost text via email
+
+// const TelCarrier = require('tel-carrier')
+// var telCarrier
+// var car
+// let lookups = require('email-to-phone');
+// telCarrier = TelCarrier.create({
+//   service: 'fonefinder.net'
+// });
+
+//beginning of carrier logic
+
+// telCarrier.lookup('9892560937', (err, res) => {
+//   console.log(res)
+//   car = res.carrierComment
+//   console.log(car);
+
+//   // let obj = lookups.lookup(car);        // fuzzy lookup
+ 
+//   // let email = lookups.mms_sms(obj.name, 9193574249);   // prefer MMS but SMS is ok
+ 
+//   // console.log(email);
+
+// });
+
+//end of carrier logic
+
+
+var zang = require('zang-node');
+var enums = zang.enums;
+
+var connector = new zang.SmsConnector({
+    accountSid: process.env.ZSID,
+    authToken: process.env.ZAUTHTOKEN
+});
+
+module.exports = {
+    //default statusCallback is 'http://mycallback.url.com'
+    // will incur $.005 per message sent 
+    sendSms: function (endNum, message, statusCallback) {
+
+        connector.sendSmsMessage({
+            to: endNum,
+            from: process.env.ZNUM,
+            body: message,
+            statusCallback: statusCallback,
+            statusCallbackMethod: enums.HttpMethod.GET,
+            allowMultiple: true
+        }).then(function (data) {
+            console.log(data);
+            return data
+        });
+        
+    },
+    // lists messages sent to the number
+    listSms: function () {
+        
+        connector.listSmsMessages({
+            to: process.env.ZNUM,
+            page: 0,
+            pageSize: 30
+        }).then(function (data) {
+            console.log(data);
+            return data
+        })
+
+
+    },
+    //view one message located by SmsSid or sid
+    viewSms: function(smsSid) {
+        connector.viewSmsMessage({ 
+            smsMessageSid: smsSid 
+        }).then(function (data) { 
+            console.log(data);
+            return data 
+        });
+    }
+
+}
+
+
+
+
