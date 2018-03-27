bd19aca91f03e79d9e0fa3e8f4f48e16e43aeba4
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-03-27 18:03:46 -0500

    basic help reply

--- textParseRoute.js
+++ textParseRoute.js
@@ -2,29 +2,38 @@ const sms = require('./controller/sms.js');
 
 
 var result;
+
+
+//placeholder for receiving messages because we need a live heroku route to acutally receive messages
+// setTimeout(function() {
+  
+// })
+
 sms.listSms().then(res => { 
   result = res
+  
   var tBody = result.sms_messages[0].body
+  var inNum = result.sms_messages[0].from
   console.log(tBody)
   tBody = tBody.split(" ")
   console.log(tBody[0])
-
-  switch(tBody[0]) {
-    case entry:
+  
+  switch(tBody[0].toLowerCase()) {
+    case "entry":
        //entry tree
       break;
-    case query:
+    case "query":
        switch(tBody[1]) {
-        case date: 
+        case "date": 
           //date querying logic
           break;
-        case keyword:
+        case "keyword":
           //keyword search logic
           break;
-        case tag:
+        case "tag":
           // search for tags/tables
           switch(tBody[3]) {
-            case id:
+            case "id":
             //look up by tag/table and ID
             break;
           }
@@ -33,9 +42,12 @@ sms.listSms().then(res => {
         
        }
       break;
-    case help:
+    case "help":
       //return help options to user
 
+      var helpString = "help commands placeholder"
+       sms.sendSms(inNum, helpString)
+       console.log("help reply sent")
   }
 
 })
