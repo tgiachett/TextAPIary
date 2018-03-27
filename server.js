46e22a060467bb4408f7e7f55347d02f08ea5166
AlexFP <florianprescott@googlemail.com>
2018-03-27 19:56:44 -0400

    router tweak, server crash fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // dependencies 
 const express = require("express"),
 	bodyParser = require("body-parser"),
-	exphbs = require("express-handlebars");
+	exphbs = require("express-handlebars"),
+	path = require("path");
 
 const db = require("./models"),
 	routes = require("./routes");
