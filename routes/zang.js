bd8e5fb10ad3f96edb267d9e75e14687f2c8d988
Alexander Florian-Prescott <31100098+EPluribusAlex@users.noreply.github.com>
2018-03-28 12:57:33 -0400

    bit of reorganization (#42)
    * server file, config file, pachages saved, index object, readme updates, dotenv implemented
    
    * changed indenting in code
    
    * readme formatting fixes
    
    * highlighted placeholder text in readme
    
    * some basic model and route structure
    
    * some user mdoel code, route tweaks
    
    * router for zang
    
    * router tweak, server crash fix
    
    * cleaned up zang route
    
    * split off zang route
    
    * remove ds_store
    
    * reorganized server with /bin/www and start script
    
    * zang.js fix

---
+++ routes/zang.js
@@ -0,0 +1,63 @@
+const models  = require("../models");
+const express = require("express");
+const router  = express.Router();
+
+router.post("/api/incoming", (req, res) => {
+  res.send("Success");
+  console.log(req.body);
+  const smsComObj = {};
+  const msgInfo = req.body;
+  const inNum = msgInfo.From;
+  // merge request body onto smsComObj
+  smsComObj.from = msgInfo.From;
+  smsComObj.id = msgInfo.SmsSid;
+  smsComObj.wholeBody = msgInfo.Body;
+  // smsComObj.date = moment()
+  const smsArr = msgInfo.Body.split(" ");
+  switch(smsArr[0].toLowerCase()) {
+    case "entry":
+      //entry tree
+      if(smsArr[2]) {
+        //if there's a tbl name
+        //make an object for the entry into that tbl
+        smsComObj.tbl = smsArr[2];
+        smsComObj.comBody = smsArr[1];
+        if(smsArr[3]) {
+          smsComObj.password = smsArr[3];
+        }
+      } else { 
+          // validation check, send error
+          const noTblErr = "Please give tbl arg after message to store in or create table";
+          sms.sendSms(inNum, noTblErr);
+      }
+      break;
+    case "query":
+      switch(smsArr[1]) {
+        case "date": 
+          //date querying logic
+          smsComObj.dateSearch = smsArr[2];
+          break;
+        case "kywrd":
+          //keyword search logic
+          smsComObj.keyWordSearch = smsArr[2];
+          break;
+        case "tbl":
+          // search for tags/tables
+          smsComObj.tblSearch = smsArr[2];
+          if(smsArr[3]) {
+            //look up by tag/table and ID if an ID is present
+            smsComObj.idSearch = smsArr[3];
+          }
+          break;       
+      }
+      break;
+    case "help":
+      //return help options to user
+      const helpString = "help commands placeholder";
+      sms.sendSms(inNum, helpString);
+      console.log("help reply sent");
+      break;
+  }
+});
+
+module.exports = router;
