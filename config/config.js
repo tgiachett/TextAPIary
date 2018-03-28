f55df40e2afdb2e0d076d5a7f8a3f4790fbbd135
AlexFP <florianprescott@googlemail.com>
2018-03-28 15:20:08 -0400

    config

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     username: "root",
     password: process.env.DB_PASS,
     database: "entries_db",
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "mysql"
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "mysql"
   },
   production: {
     username: "root",
-    password: null,
-    database: "entries_db",
+    password: process.env.DB_PASS,
+    database: "localhost",
     host: "127.0.0.1",
     dialect: "mysql"
   }
