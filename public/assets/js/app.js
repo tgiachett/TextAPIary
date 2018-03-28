dab7f4bae7403988465fc21a56a704db30b75ea0
paulaperoutka <paulaperoutka@gmail.com>
2018-03-28 12:05:30 -0400

    add app.js for client side render

---
+++ public/assets/js/app.js
@@ -0,0 +1,13 @@
+// $(function () {
+//     $('#SOME-button').click(function(event){
+//         event.preventDefault();
+//         var id = $(this).attr("SOME-id");
+//         $.ajax(some stuff, {
+
+//         }).then(
+//             function () {
+//                 location.reload();
+//             }
+//         );
+//     });
+
