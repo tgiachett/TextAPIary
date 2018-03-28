4631b3534bfae0647d3684fbc8fd2b7f61b03bd9
AlexFP <florianprescott@googlemail.com>
2018-03-28 16:41:02 -0400

    index.js fix

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ let sequelize;
 
 if(process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
       port:     match[4],
