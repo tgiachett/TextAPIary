db397b4c6d1bb3a42e6af03df03d2c461d0856a9
Alexander Florian-Prescott <31100098+EPluribusAlex@users.noreply.github.com>
2018-03-29 18:39:03 -0400

    revert! (#49)
    * server file, config file, pachages saved, index object, readme updates, dotenv implemented
    
    * changed indenting in code
    
    * readme formatting fixes
    
    * highlighted placeholder text in readme
    
    * some basic model and route structure
    
    * some user mdoel code, route tweaks
    
    * router for zang
    
    * router tweak, server crash fix
    
    * cleaned up zang route
    
    * split off zang route
    
    * remove ds_store
    
    * reorganized server with /bin/www and start script
    
    * zang.js fix
    
    * minor config tweak
    
    * some route tests
    
    * tweaks
    
    * config
    
    * temp disable dotenv
    
    * postgresql
    
    * removed line from app.js
    
    * postgresstweaks
    
    * index.js fix
    
    * index tweak
    
    * some model tweaks
    
    * some route tweaks
    
    * revert and add some api tweaks

--- models/entry.js
+++ models/entry.js
@@ -3,12 +3,6 @@
 
 module.exports = function(sequelize, DataTypes) {
   const Entry = sequelize.define("Entry", {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    comBody: {
       type: DataTypes.TEXT,
       alloNull: false,
       validate: { len: [1] },
@@ -17,29 +11,8 @@ module.exports = function(sequelize, DataTypes) {
     category: {
       type: DataTypes.STRING,
       defaultValue: "Public"
-    },
-    tbl: {
-      type: DataTypes.STRING,
-      alloNull: false
-    },
-    tblPass: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    SmsSid: {
-      type: DataTypes.STRING,
-      alloNull: false
-    },
-    from: {
-      type: DataTypes.INT,
-      allowNull: false
-    },
-    wholeBody: {
-      type: DatTypes.TEXT
-    },
-
-
-  },
+    }
+  }, 
   {
     classMethods: {
       associate: (models) => {
