70ff84853d3524cdf00e4ac53ffed89d640f33d3
Alexander Florian-Prescott <31100098+EPluribusAlex@users.noreply.github.com>
2018-03-29 21:11:43 -0400

    tinker (#59)
    * server file, config file, pachages saved, index object, readme updates, dotenv implemented
    
    * changed indenting in code
    
    * readme formatting fixes
    
    * highlighted placeholder text in readme
    
    * some basic model and route structure
    
    * some user mdoel code, route tweaks
    
    * router for zang
    
    * router tweak, server crash fix
    
    * cleaned up zang route
    
    * split off zang route
    
    * remove ds_store
    
    * reorganized server with /bin/www and start script
    
    * zang.js fix
    
    * minor config tweak
    
    * some route tests
    
    * tweaks
    
    * config
    
    * temp disable dotenv
    
    * postgresql
    
    * removed line from app.js
    
    * postgresstweaks
    
    * index.js fix
    
    * index tweak
    
    * some model tweaks
    
    * some route tweaks
    
    * revert and add some api tweaks
    
    * sms required in zang
    
    * sms
    
    * zang tweak
    
    * fixed entry.js
    
    * zang id
    
    * zang from
    
    * From
    
    * entry tweak
    
    * zang smsComObj
    
    * zang

--- models/entry.js
+++ models/entry.js
@@ -2,15 +2,36 @@
 
 module.exports = function(sequelize, DataTypes) {
   const Entry = sequelize.define("Entry", {
-    body: {
-      type: DataTypes.TEXT,
-      alloNull: false,
-      validate: { len: [1] },
-      description: DataTypes.TEXT
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    comBody: {
+      type: DataTypes.STRING
     },
     category: {
       type: DataTypes.STRING,
       defaultValue: "Public"
+    },
+    tbl: {
+      type: DataTypes.STRING,
+      alloNull: false
+    },
+    tblPass: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    SmsSid: {
+      type: DataTypes.STRING,
+      alloNull: false
+    },
+    from: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    wholeBody: {
+      type: DataTypes.TEXT
     }
   }, 
   {
